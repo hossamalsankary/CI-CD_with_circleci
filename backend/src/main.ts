5ccb59a8f8ea69c828f228aedc29b22b54c00060
hossam Alsankary <hossamalsankary@gmail.com>
2022-09-04 23:09:39 +0200

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,18 +18,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
     logger,
   });
+  app.Cors();
   app.enableCors();
+  
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   const apiVersionPrefix: string = process.env.API_VERSION || 'api';
   app.setGlobalPrefix(apiVersionPrefix);
   app.useGlobalInterceptors(new TransformInterceptor());
   const options = new DocumentBuilder()
-    .setTitle('Glee2')
-    .setDescription('Glee2 API')
-    .setVersion('1.0')
-    .addTag('customTag')
-    .setBasePath(apiVersionPrefix)
-    .addBearerAuth()// here is an intentional compile error. Remove the "x" and the backend should compile.
+  .setTitle('Glee2')
+  .setDescription('Glee2 API')
+  .setVersion('1.0')
+  .addTag('customTag')
+  .setBasePath(apiVersionPrefix)
+  .addBearerAuth()// here is an intentional compile error. Remove the "x" and the backend should compile.
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
