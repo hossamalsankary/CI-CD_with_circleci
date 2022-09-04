4573ee62d6174b3adc467a9efd238038d8fe138b
hossam Alsankary <hossamalsankary@gmail.com>
2022-09-04 07:12:12 +0200

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,18 +11,16 @@ import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
 async function bootstrap() {
+  
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
   logger.info(JSON.stringify(process.env));
-  const server = express(
-
-  );
-  server.use(cors());
-
+  const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server), {
     logger,
   });
-  app.use(cors())
+    app.use(cors());
+
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   const apiVersionPrefix: string = process.env.API_VERSION || 'api';
   app.setGlobalPrefix(apiVersionPrefix);
@@ -38,18 +36,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
   const config: ConfigService = app.get('ConfigService');
-  const whitelist = config.CORS_WHITELIST;
-  const corsOptions = {
-    origin(origin, callback) {
-      const isOriginAllowed = whitelist.indexOf(origin) !== -1;
-      const allowAccessAnyway = whitelist.length === 0;
-      if (isOriginAllowed || allowAccessAnyway) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
+  
+  app.use(cors());
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
